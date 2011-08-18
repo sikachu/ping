0418d00a1cca2275ec39e1aa028a086fe6e04886
Prem Sichanugrist <s@sikachu.com>
2011-08-18 11:00:45 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pingpong::Application.config.secret_token = 'd66773860bcc283a97643d3f533f906196819e7508bc98dea3f937cacd6ac9e287b12d0077f430b3d06dd5fcfc3fb7667ec386fdee1b1d7563bc7bd67fd8b16a'
